9dec9a61e238e2fc0bffd3f13ccee26f4055df3e
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2025-10-03 14:22:23 +0100

    fix: focus on undotree when toggled

--- nvim/plugin/undotree.lua
+++ nvim/plugin/undotree.lua
@@ -1,5 +1,7 @@
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+vim.g.undotree_SetFocusWhenToggle = true
+
 local map = require('me.keymap').map
 map("n", "<leader>u", function() vim.cmd.UndotreeToggle() end, { desc = "Toffle undotree buffer" })
