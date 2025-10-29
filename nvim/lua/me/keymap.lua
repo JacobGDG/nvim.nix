e24aa7ca19606d96e4d79fc93251bb1876d2f7df
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2025-10-29 22:17:14 +0000

    refactor: remove pointless localmap function

--- nvim/lua/me/keymap.lua
+++ nvim/lua/me/keymap.lua
@@ -9,13 +9,4 @@ M.map = function(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, options)
 end
 
-M.localmap = function(mode, lhs, rhs, opts, bufnr)
-  local options = { buffer = bufnr }
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
-  end
-
-  map(mode, lhs, rhs, options)
-end
-
 return M
--- nvim/plugin/lsp/keymaps.lua
+++ nvim/plugin/lsp/keymaps.lua
@@ -1,6 +1,6 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
-local localmap = require('me.keymap').localmap
+local map = require('me.keymap').map
 
 autocmd('LspAttach', {
   group = augroup('UserLspConfig', {}),
@@ -18,27 +18,27 @@ autocmd('LspAttach', {
     vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
     -- [g]o to keymaps
-    localmap('n', 'gD', vim.lsp.buf.declaration, { desc = 'lsp [g]o to [D]eclaration' }, bufnr)
-    localmap('n', 'gd', vim.lsp.buf.definition, { desc = 'lsp [g]o to [d]efinition' }, bufnr)
-    localmap('n', 'gt', vim.lsp.buf.type_definition, { desc = 'lsp [g]o to [t]ype definition' }, bufnr)
-    localmap('n', 'gi', vim.lsp.buf.implementation, { desc = 'lsp [g]o to [i]mplementation' }, bufnr)
-    localmap('n', 'gr', vim.lsp.buf.references, { desc = 'lsp [g]et [r]eferences' }, bufnr)
+    map('n', 'gD', vim.lsp.buf.declaration, { desc = 'lsp [g]o to [D]eclaration', buffer = bufnr })
+    map('n', 'gd', vim.lsp.buf.definition, { desc = 'lsp [g]o to [d]efinition', buffer = bufnr })
+    map('n', 'gt', vim.lsp.buf.type_definition, { desc = 'lsp [g]o to [t]ype definition', buffer = bufnr })
+    map('n', 'gi', vim.lsp.buf.implementation, { desc = 'lsp [g]o to [i]mplementation', buffer = bufnr })
+    map('n', 'gr', vim.lsp.buf.references, { desc = 'lsp [g]et [r]eferences', buffer = bufnr })
 
-    localmap('n', 'K', vim.lsp.buf.hover, { desc = '[lsp] hover' }, bufnr)
+    map('n', 'K', vim.lsp.buf.hover, { desc = '[lsp] hover', buffer = bufnr })
 
-    localmap('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'lsp [r]e[n]ame' }, bufnr)
-    localmap('n', '<M-CR>', vim.lsp.buf.code_action, { desc = '[lsp] code action' }, bufnr)
-    -- localmap('n', '<M-l>', vim.lsp.codelens.run, { desc = '[lsp] run code lens' }, bufnr)
-    -- localmap('n', '<leader>cr', vim.lsp.codelens.refresh, { desc = 'lsp [c]ode lenses [r]efresh' }, bufnr)
-    localmap('n', '<leader>F', function()
+    map('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'lsp [r]e[n]ame', buffer = bufnr })
+    map('n', '<M-CR>', vim.lsp.buf.code_action, { desc = '[lsp] code action', buffer = bufnr })
+    -- localmap('n', '<M-l>', vim.lsp.codelens.run, { desc = '[lsp] run code lens', buffer = bufnr })
+    -- localmap('n', '<leader>cr', vim.lsp.codelens.refresh, { desc = 'lsp [c]ode lenses [r]efresh', buffer = bufnr })
+    map('n', '<leader>F', function()
 
       vim.lsp.buf.format { async = true }
-    end, { desc = '[lsp] [f]ormat buffer' }, bufnr)
+    end, { desc = '[lsp] [f]ormat buffer', buffer = bufnr })
     if client and client.server_capabilities.inlayHintProvider then
-      localmap('n', '<leader>h', function()
+      map('n', '<leader>h', function()
         local current_setting = vim.lsp.inlay_hint.is_enabled { bufnr = bufnr }
         vim.lsp.inlay_hint.enable(not current_setting, { bufnr = bufnr })
-      end, { desc = '[lsp] toggle inlay hints' }, bufnr)
+      end, { desc = '[lsp] toggle inlay hints', buffer = bufnr })
     end
 
     -- Auto-refresh code lenses
