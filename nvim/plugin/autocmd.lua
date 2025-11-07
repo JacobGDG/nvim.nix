ef566f99fb94afbdc79cc9609b99a8b9cffdc8a1
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2025-11-07 09:59:11 +0000

    docs: add descriptions to autocommands

--- nvim/plugin/autocmd.lua
+++ nvim/plugin/autocmd.lua
@@ -5,10 +5,11 @@ local my_group = augroup('Mine', {})
 autocmd('TextYankPost', {
   group = my_group,
   pattern = '*',
+  desc = 'Highlight yanked text for clarity',
   callback = function()
     vim.highlight.on_yank {
       higroup = 'IncSearch',
-      timeout = 1000,
+      timeout = 200,
     }
   end,
 })
@@ -16,15 +17,18 @@ autocmd('TextYankPost', {
 autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave' }, {
   group = my_group,
   pattern = '*',
+  desc = 'Show relative numbers when in buffer for jumping',
   command = 'set relativenumber',
 })
 autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter' }, {
   group = my_group,
   pattern = '*',
+  desc = 'Show absolute numbers when not in buffer for referencing',
   command = 'set norelativenumber',
 })
 
 autocmd({ 'VimResized' }, {
   group = my_group,
+  desc = 'Auto size windows on terminal resize',
   command = 'wincmd =',
 })
