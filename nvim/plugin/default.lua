c6f877f2f220624a741a89005ffae6392079dbce
JacobGDG <10035081+JacobGDG@users.noreply.github.com>
2026-04-08 11:44:19 +0100

    perf: setup luasnip on insert

--- nvim/plugin/default.lua
+++ nvim/plugin/default.lua
@@ -1,5 +1,14 @@
--- Many plugins need "setup" dump them here
-require('gitsigns').setup()
-require('luasnip').setup()
-require('luasnip.loaders.from_vscode').lazy_load()
+local my_group = vim.api.nvim_create_augroup('Plugins', {})
+local autocmd = vim.api.nvim_create_autocmd
+
 require('mini.icons').setup()
+
+autocmd('InsertEnter', {
+  group = my_group,
+  desc = 'Load buffer based plugin',
+  once = true,
+  callback = function()
+    require('luasnip').setup()
+    require('luasnip.loaders.from_vscode').lazy_load()
+  end,
+})
